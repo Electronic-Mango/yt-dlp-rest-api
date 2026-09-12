4d0c7ac12d9f02a1bfe5254592ceb4d79156072e
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2026-09-12 21:21:43 +0200

    Simplify the implementation

--- src/main.py
+++ src/main.py
@@ -10,10 +10,13 @@ from starlette.background import BackgroundTask
 from yt_dlp import YoutubeDL, match_filter_func
 
 load_dotenv()
-DURATION_MAX = getenv("DURATION_MAX")
-FORMAT = getenv("FORMAT")
-FORMAT_SORT = getenv("FORMAT_SORT")
+
+DEFAULT_MAX_DURATION = getenv("MAX_DURATION")
+DEFAULT_FORMAT = getenv("FORMAT")
+DEFAULT_FORMAT_SORT = getenv("FORMAT_SORT")
+
 ENCODING = "UTF-8"
+THUMBNAIL_PARAMS = {"writethumbnail": True, "skip_download": True}
 
 app = FastAPI()
 
@@ -21,47 +24,34 @@ app = FastAPI()
 @app.get("/download")
 def download(
     video_url: str,
-    duration_max: int | None = DURATION_MAX,
-    format: str | None = FORMAT,
-    format_sort: str | None = FORMAT_SORT,
+    max_duration: int | None = DEFAULT_MAX_DURATION,
+    format: str | None = DEFAULT_FORMAT,
+    format_sort: str | None = DEFAULT_FORMAT_SORT,
 ) -> Response:
     filename = str(uuid4())
-    file = download_video(video_url, filename, duration_max, format, format_sort)
-    file = file or download_thumbnail(video_url, filename)
+    params = video_params(max_duration, format, format_sort)
+    file = download_file(video_url, filename, params)
+    file = file or download_file(video_url, filename, THUMBNAIL_PARAMS)
     return FileResponse(file, background=BackgroundTask(file.unlink))
 
 
-def download_video(
-    video_url: str,
-    filename: str,
-    duration_max: int | None,
+def video_params(
+    max_duration: int | None,
     format: str | None,
     format_sort: str | None,
-) -> Path | None:
-    params = prepare_target_params(filename)
+) -> dict[str, Any]:
+    params = {}
     if format is not None:
         params["format"] = format
     if format_sort is not None:
         params["format_sort"] = [format_sort]
-    if duration_max is not None:
-        params["match_filter"] = match_filter_func(f"duration<={duration_max}")
-    return download_file(params, video_url, filename)
+    if max_duration:
+        params["match_filter"] = match_filter_func(f"duration<={max_duration}")
+    return params
 
 
-def download_thumbnail(video_url: str, filename: str) -> Path | None:
-    params = prepare_target_params(filename) | {
-        "writethumbnail": True,
-        "skip_download": True,
-    }
-    return download_file(params, video_url, filename)
-
-
-def prepare_target_params(filename: str) -> dict[str, str]:
-    return {"outtmpl": f"{filename}.%(ext)s"}
-
-
-def download_file(params: dict[str, Any], video_url: str, filename: str) -> Path | None:
-    with YoutubeDL(params) as ytdl:
+def download_file(video_url: str, filename: str, params: dict[str, Any]) -> Path | None:
+    with YoutubeDL({"outtmpl": f"{filename}.%(ext)s"} | params) as ytdl:
         ytdl.download(video_url)
     return find_downloaded_file(filename)
 
